072526e532ef13c4eb3d58f6c97e05bc59adb6ea
Oleksiy <do.gen.enterprise@gmail.com>
2025-01-16 14:48:47 +0200

    update envalid config check for dev and prod

--- src/tools/validateEnv/validateEnv.ts
+++ src/tools/validateEnv/validateEnv.ts
@@ -4,10 +4,16 @@ export default function validateEnv() {
     if (process.env.NODE_ENV?.trim() === "dev") {
       cleanEnv(process.env, {
         PORT: str(),
+        JWT_SECRET: str(),
+        MONGO_URL: str(),
+        MONGO_DB_NAME: str(),
       });
     } else if (process.env.NODE_ENV?.trim() === "prod") {
       cleanEnv(process.env, {
         PORT: str(),
+        JWT_SECRET: str(),
+        MONGO_URL: str(),
+        MONGO_DB_NAME: str(),
       });
     } else if (process.env.NODE_ENV?.trim() === "local") {
       cleanEnv(process.env, {
