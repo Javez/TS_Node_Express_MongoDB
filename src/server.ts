9ae5ecc5d2d57a81c988d4b5e1cb7962172d7a68
Алексей Данилов <mr.javez@ukr.net>
2025-01-20 10:45:27 +0200

    setup new controllers in main file

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,17 @@ import App from "./app";
 import PostController from "./controllers/posts/post.controller";
 import validateEnv from "./tools/validateEnv/validateEnv";
 import AuthController from "./controllers/auth/auth.controller";
+import ReportController from "./controllers/report/report.controller";
+import UserController from "./controllers/users/user.controller";
 
 validateEnv();
 const app = new App(
-  [new AuthController(), new PostController()],
+  [
+    new AuthController(),
+    new UserController(),
+    new PostController(),
+    new ReportController(),
+  ],
   Number(process.env.PORT)
 );
 app.listen();
